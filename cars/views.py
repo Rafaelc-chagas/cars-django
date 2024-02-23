6acf3608e0b09146fa4f920cf3a51183660edea7
Rafael Chagas <rafaelcchagas12@gmail.com>
2024-02-23 14:27:40 -0300

    my first commit

---
+++ cars/views.py
@@ -0,0 +1,48 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from cars.models import Car
+from cars.forms import CarModelForm
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        if search:
+            cars = cars.filter(model__icontains=search)
+        return cars
+    
+
+class NewCarCreateView(LoginRequiredMixin, CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
+    login_url = '/login/'
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+
+
+class CarUpdateView(LoginRequiredMixin, UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+    login_url = '/login/'
+
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
+    
+
+class CarDeleteView(LoginRequiredMixin, DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
+    success_url = '/cars/'
+    login_url = '/login/'
